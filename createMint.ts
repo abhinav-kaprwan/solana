08c2b048a3a6bedd306cc6c8fdef4113a600cd94
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2024-07-27 12:41:40 +0530

    minted tokens

--- createMint.ts
+++ createMint.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import {createAccount, createMint, getOrCreateAssociatedTokenAccount} from '@solana/spl-token'
+import {mintTo, getOrCreateAssociatedTokenAccount, getAssociatedTokenAddress} from '@solana/spl-token'
 import { clusterApiUrl, Connection } from '@solana/web3.js';
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
@@ -12,7 +12,8 @@ async function main() {
     const payer = loadKeyPair("SECRET_KEY")
     const tokenKeyPair = loadKeyPair("TOKEN_SECRET_KEY")
     const tokenAccountKeypair = loadKeyPair("TOKEN_ACCOUNT_SECRET_KEY")
-
+    const mint = tokenKeyPair.publicKey
+    const decimal = 9
     // used to create token mint address
     // const tokenMintAddress = await createMint(connection,payer,payer.publicKey,payer.publicKey,9,tokenKeyPair);
     // console.log(tokenMintAddress.toBase58())
@@ -21,10 +22,18 @@ async function main() {
     // // if we didn't provide key pair to token account then it becomes associate token account 
     // console.log(tokenAccount.toBase58())
 
-    const ata = await getOrCreateAssociatedTokenAccount(connection,payer,tokenKeyPair.publicKey,payer.publicKey);
-    console.log(ata.address.toBase58())
+    // const ata = await getOrCreateAssociatedTokenAccount(connection,payer,tokenKeyPair.publicKey,payer.publicKey);
+    // console.log(ata.address.toBase58())
 
     //Under the hood this function is checking firstly either the associated token account is created if not then it is creating it
+
+    const ata = await getAssociatedTokenAddress(mint,payer.publicKey)
+    console.log(ata)
+    const amount = 3*10**decimal
+    const sigx = await mintTo(connection,payer,mint,ata,payer.publicKey,amount)
+    console.log(sigx)
+
+
 }
 
 main();
